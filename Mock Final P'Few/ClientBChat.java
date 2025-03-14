b9892503a8a3eb76ab322e88a3a0cf6b840b8f3f
TobyNaja <107453516+TobyNaja@users.noreply.github.com>
2025-03-14 22:36:17 +0700

    Add files via upload

---
+++ Mock Final P'Few/ClientBChat.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pfew;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientBChat {
+    private JFrame fr;
+    private JPanel pn;
+    private JTextField txt;
+    private JTextArea ar;
+    private JButton bn;
+    private EventB ev;
+    
+    public ClientBChat(){
+        fr = new JFrame("ClientBChat");
+        txt = new JTextField(45);
+        ar = new JTextArea(20,45);
+        bn = new JButton("Send");
+        ar.setEditable(false);
+        ev = new EventB(this);
+        
+        fr.setLayout(new BorderLayout());
+        fr.add(ar,BorderLayout.NORTH);
+        fr.add(txt,BorderLayout.WEST);
+        fr.add(bn,BorderLayout.EAST);
+        
+        fr.addWindowListener(ev);
+        bn.addActionListener(ev);
+        
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
+    }
+
+    public JFrame getFr() {
+        return fr;
+    }
+
+    public void setFr(JFrame fr) {
+        this.fr = fr;
+    }
+
+    public JPanel getPn() {
+        return pn;
+    }
+
+    public void setPn(JPanel pn) {
+        this.pn = pn;
+    }
+
+    public JTextField getTxt() {
+        return txt;
+    }
+
+    public void setTxt(JTextField txt) {
+        this.txt = txt;
+    }
+
+    public JTextArea getAr() {
+        return ar;
+    }
+
+    public void setAr(JTextArea ar) {
+        this.ar = ar;
+    }
+
+    public JButton getBn() {
+        return bn;
+    }
+
+    public void setBn(JButton bn) {
+        this.bn = bn;
+    }
+    
+    
+}
+
---
+++ Mock Final P'Few/EventA.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pfew;
+
+import java.awt.event.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.*;
+
+public class EventA implements ActionListener, WindowListener {
+    private ClientAChat gui;
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private static File file = new File("chat_history.dat");
+    
+    public EventA(ClientAChat gui){
+        this.gui = gui;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Send")){
+            String txt = gui.getTxt().getText();
+            String time = dtf.format(LocalDateTime.now());
+            gui.getAr().append(time + " " + "A :" + txt+"\n");
+            gui.getTxt().setText("");
+        }
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        if (file.exists()){
+            try (FileInputStream fin = new FileInputStream(file);
+                    DataInputStream din = new DataInputStream(fin);){
+                String txt = din.readUTF();
+                if (!txt.isEmpty()){
+                    gui.getAr().append(txt);
+                }
+            }catch (IOException ex) {
+                System.exit(0);
+            }
+        }
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        try (FileOutputStream fout = new FileOutputStream(file);
+                DataOutputStream dout = new DataOutputStream(fout)){
+            dout.writeUTF(gui.getAr().getText());
+        } catch (IOException ex) {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+    }
+}
